3345c6e8b983f4ced90a550ea76baf152fc5be52
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-07-29 12:36:42 -0400

    Fix env variable typo in staging config

--- functions/settings.py
+++ functions/settings.py
@@ -7,7 +7,8 @@ from cidc_api.config import get_sqlalchemy_database_uri
 # in the environment variable GCP_PROJECT. If this
 # variable isn't set, then we're not running in GCP.
 # See: https://cloud.google.com/functions/docs/env-var
-if not os.environ.get("GCP_PROJECT"):
+GCP_PROJECT = os.environ.get("GCP_PROJECT")
+if not GCP_PROJECT:
     from dotenv import load_dotenv
 
     # We're running locally, so load config from .env
@@ -17,3 +18,8 @@ TESTING = os.environ.get("TESTING")
 SQLALCHEMY_DATABASE_URI = get_sqlalchemy_database_uri(TESTING)
 GOOGLE_UPLOAD_BUCKET = os.environ.get("GOOGLE_UPLOAD_BUCKET")
 GOOGLE_DATA_BUCKET = os.environ.get("GOOGLE_DATA_BUCKET")
+
+# Check for configuration that must be defined if we're running in GCP
+if GCP_PROJECT:
+    assert GOOGLE_DATA_BUCKET is not None
+    assert GOOGLE_UPLOAD_BUCKET is not None
