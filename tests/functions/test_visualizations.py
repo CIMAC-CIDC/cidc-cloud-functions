9b8b8a7429a96124af29dd5865e8d57d74a5a089
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-12-09 11:35:42 -0500

    deploy staging to prod (#83)
    * Save Clustergrammer config as json, not string (#80)
    
    * Add failing test
    
    * Fix failing test
    
    * Use net.viz directly
    
    * Add sample metadata categories to clustergrammer visualization and normalize NPX data (#81)
    
    * Add metadata categories to NPX clustergrammer visualization
    
    * Update test to do end-to-end check of CG npx transform
    
    * Remove unused _npx_add_categories method
    
    * Fix BytesIO/StringIO TypeError currently causing vis_preprocessing to crash (#82)
    
    * cidc-api-modules~=0.12.1

--- tests/functions/test_visualizations.py
+++ tests/functions/test_visualizations.py
@@ -1,4 +1,5 @@
 import os
+from io import BytesIO, StringIO
 from unittest.mock import MagicMock
 
 import pytest
@@ -10,15 +11,32 @@ from functions.visualizations import (
     DownloadableFiles,
     _ClustergrammerTransform,
     _npx_to_dataframe,
+    _get_blob_as_stream,
 )
 
 from tests.util import make_pubsub_event
 
 DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+NPX_PATH = os.path.join(DATA_DIR, "fake_npx.xlsx")
 
 
-def test_vis_preprocessing(monkeypatch):
-    """Test control flow for the vis_preprocessing function"""
+@pytest.fixture
+def metadata_df():
+    """Fake participants/sample metadata for `fake_npx.xlsx`"""
+    metadata_df = pd.DataFrame.from_dict(
+        {
+            "cimac_id": ["CTTTTPPS1.01", "CTTTTPPS2.01"],
+            "cimac_participant_id": ["CTTTTPP", "CTTTTPP"],
+            "cohort_name": ["Arm_A", "Arm_A"],
+            "collection_event_name": ["Event1", "Event2"],
+        }
+    )
+    metadata_df.set_index("cimac_id", inplace=True)
+    return metadata_df
+
+
+def test_npx_clustergrammer_end_to_end(monkeypatch, metadata_df):
+    """Test the NPX-clustergrammer transform."""
     # Test no file found
     monkeypatch.setattr(DownloadableFiles, "find_by_id", lambda *args, **kwargs: None)
     with pytest.raises(Exception, match="No downloadable file"):
@@ -34,31 +52,82 @@ def test_vis_preprocessing(monkeypatch):
 
     # Mock GCS call
     gcs_blob = MagicMock()
-    get_file_from_gcs = MagicMock()
-    get_file_from_gcs.return_value = gcs_blob
+    _get_blob_as_stream = MagicMock()
+    fake_npx = open(NPX_PATH, "rb")
+    _get_blob_as_stream.return_value = fake_npx
     monkeypatch.setattr(
-        functions.visualizations, "_get_file_from_gcs", get_file_from_gcs
+        functions.visualizations, "_get_blob_as_stream", _get_blob_as_stream
     )
 
-    # Mock Clustgrammer preprocessing
-    cg_json = {"foo": "bar"}
-    cg_npx_transform = MagicMock()
-    cg_npx_transform.return_value = cg_json
-    monkeypatch.setattr(_ClustergrammerTransform, "npx", cg_npx_transform)
+    # Mock metadata_df
+    _get_metadata_df = MagicMock()
+    _get_metadata_df.return_value = metadata_df
+    monkeypatch.setattr(functions.visualizations, "_get_metadata_df", _get_metadata_df)
 
     vis_preprocessing(make_pubsub_event("1"), {})
     find_by_id.assert_called_once()
-    get_file_from_gcs.assert_called_once()
+    _get_blob_as_stream.assert_called_once()
+    _get_metadata_df.assert_called_once()
 
-    assert npx_record.clustergrammer == cg_json
+    # Check contents of the clustergrammer output
+    row_names = [row["name"] for row in npx_record.clustergrammer["row_nodes"]]
+    col_names = [col["name"] for col in npx_record.clustergrammer["col_nodes"]]
+    col_cats = [
+        (col["cat-0"], col["cat-1"], col["cat-2"])
+        for col in npx_record.clustergrammer["col_nodes"]
+    ]
+
+    # Based on the contents of fake_npx.xlsx...
+    assert row_names == ["Assay1", "Assay2"]
+    assert col_names == [
+        "CIMAC Sample ID: CTTTTPPS1.01",
+        "CIMAC Sample ID: CTTTTPPS2.01",
+    ]
+
+    # Based on the construction of metadata_df...
+    assert col_cats == [
+        ("Participant ID: CTTTTPP", "Cohort: Arm_A", "Collection Event: Event1"),
+        ("Participant ID: CTTTTPP", "Cohort: Arm_A", "Collection Event: Event2"),
+    ]
+
+    fake_npx.close()
+
+
+def test_clustergrammer_transform(metadata_df):
+    """Smoke test for the clustergrammer transform"""
+    cg = _ClustergrammerTransform()
+
+    with open(NPX_PATH, "rb") as fake_npx:
+        assert isinstance(cg.npx(fake_npx, metadata_df), dict)
 
 
 def test_npx_to_dataframe():
     """Extract data from a fake NPX file"""
-    with open(os.path.join(DATA_DIR, "fake_npx.xlsx"), "rb") as fake_npx:
+    with open(NPX_PATH, "rb") as fake_npx:
         npx_df = _npx_to_dataframe(fake_npx)
 
     expected_df = pd.DataFrame(
         {"CTTTTPPS1.01": [1, -1], "CTTTTPPS2.01": [-1, 1]}, index=["Assay1", "Assay2"]
     )
     assert expected_df.equals(npx_df)
+
+
+def test_get_blob_as_stream(monkeypatch):
+    """Ensure GCS blob utility works as expected"""
+    blob_str = "foo bar"
+    blob_bytes = blob_str.encode()
+    get_blob_bytes = MagicMock()
+    get_blob_bytes.return_value = blob_bytes
+    monkeypatch.setattr(
+        functions.visualizations, "__download_blob_bytes", get_blob_bytes
+    )
+
+    # as BytesIO
+    stream = _get_blob_as_stream("")
+    assert isinstance(stream, BytesIO)
+    assert stream.read() == blob_bytes
+
+    # as StringIO
+    stream = _get_blob_as_stream("", as_string=True)
+    assert isinstance(stream, StringIO)
+    assert stream.read() == blob_str
