f39c330b19ab7c3409a2733d053258fabd81133d
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-12-11 16:09:08 -0500

    parallelize GCS blob copying in ingest_upload

--- deploy/pubsub.sh
+++ deploy/pubsub.sh
@@ -4,10 +4,14 @@
 ENTRYPOINT=$1
 TOPIC=$2
 ENVFILE=$3
+TIMEOUT=$4
+MEMORY=$5
 PROJECT=$(gcloud config get-value project)
 
 gcloud functions deploy $ENTRYPOINT \
     --runtime python37 \
     --trigger-topic $TOPIC \
     --env-vars-file $ENVFILE \
-    --project $PROJECT
+    --project $PROJECT \
+    --timeout $TIMEOUT \
+    --memory $MEMORY
