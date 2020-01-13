dbc3f6aa93ff332d1b4f5965130ecd745d12429e
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-01-13 11:36:11 -0500

    Add missing CFn import to main.py (#103)

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from functions import (
     store_auth0_logs,
     vis_preprocessing,
     derive_files_from_manifest_upload,
+    derive_files_from_assay_or_analysis_upload,
 )
 
 from flask import Flask, request, jsonify
@@ -16,6 +17,7 @@ topics_to_functions = {
     "uploads": ingest_upload,
     "artifact_upload": vis_preprocessing,
     "patient_sample_update": derive_files_from_manifest_upload,
+    "assay_or_analysis_upload": derive_files_from_assay_or_analysis_upload,
     "daily_cron": store_auth0_logs,
     "emails": send_email,
 }
