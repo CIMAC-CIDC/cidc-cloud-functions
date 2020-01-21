a007b747d4d443447678ebb5f4eb8009974be87d
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-01-21 10:48:16 -0500

    CyTOF summary clustergrammer transform

--- tests/functions/test_visualizations.py
+++ tests/functions/test_visualizations.py
@@ -10,6 +10,7 @@ from functions.visualizations import (
     vis_preprocessing,
     DownloadableFiles,
     _ClustergrammerTransform,
+    _cytof_summary_to_dataframe,
     _npx_to_dataframe,
 )
 
@@ -17,6 +18,7 @@ from tests.util import make_pubsub_event
 
 DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
 NPX_PATH = os.path.join(DATA_DIR, "fake_npx.xlsx")
+CYTOF_PATH = os.path.join(DATA_DIR, "fake_cytof_summary.csv")
 
 
 @pytest.fixture
@@ -144,12 +146,79 @@ def test_npx_clustergrammer_end_to_end(monkeypatch, metadata_df):
     fake_npx.close()
 
 
-def test_clustergrammer_transform(metadata_df):
-    """Smoke test for the clustergrammer transform"""
-    cg = _ClustergrammerTransform()
+@pytest.mark.parametrize(
+    "data_format",
+    ("cell counts compartment", "cell counts assignment", "cell counts profiling"),
+)
+def test_cytof_clustergrammer_end_to_end(monkeypatch, metadata_df, data_format):
+    """Test the CyTOF-clustergrammer transform."""
+    # Test no file found
+    monkeypatch.setattr(
+        DownloadableFiles, "get_by_object_url", lambda *args, **kwargs: None
+    )
+    with pytest.raises(Exception, match="No downloadable file"):
+        vis_preprocessing(make_pubsub_event("foo/bar"), {})
 
-    with open(NPX_PATH, "rb") as fake_npx:
-        assert isinstance(cg.npx(fake_npx, metadata_df), dict)
+    # Mock a CyTOF summary downloadable file record
+    cytof_record = MagicMock()
+    cytof_record.object_url = "foo"
+    cytof_record.data_format = data_format
+    get_by_object_url = MagicMock()
+    get_by_object_url.return_value = cytof_record
+    monkeypatch.setattr(DownloadableFiles, "get_by_object_url", get_by_object_url)
+
+    # Mock GCS call
+    gcs_blob = MagicMock()
+    _get_blob_as_stream = MagicMock()
+    fake_cytof = open(CYTOF_PATH, "rb")
+    _get_blob_as_stream.return_value = fake_cytof
+    monkeypatch.setattr(
+        functions.visualizations, "get_blob_as_stream", _get_blob_as_stream
+    )
+
+    # Mock metadata_df
+    _get_metadata_df = MagicMock()
+    _get_metadata_df.return_value = metadata_df
+    monkeypatch.setattr(functions.visualizations, "_get_metadata_df", _get_metadata_df)
+
+    vis_preprocessing(make_pubsub_event("1"), {})
+    get_by_object_url.assert_called_once()
+    _get_blob_as_stream.assert_called_once()
+    _get_metadata_df.assert_called_once()
+
+    # Check contents of the clustergrammer output
+    row_names = [row["name"] for row in cytof_record.clustergrammer["row_nodes"]]
+    col_names = [col["name"] for col in cytof_record.clustergrammer["col_nodes"]]
+    col_cats = [
+        (col["cat-0"], col["cat-1"], col["cat-2"])
+        for col in cytof_record.clustergrammer["col_nodes"]
+    ]
+
+    # Based on the contents of fake_cytof_summary.csv...
+    assert row_names == ["cell1", "cell2"]
+    assert col_names == [
+        "CIMAC Sample ID: CTTTTPPS1.01",
+        "CIMAC Sample ID: CTTTTPPS2.01",
+    ]
+
+    # Based on the construction of metadata_df...
+    assert col_cats == [
+        ("Participant ID: CTTTTPP", "Cohort: Arm_A", "Collection Event: Event1"),
+        ("Participant ID: CTTTTPP", "Cohort: Arm_A", "Collection Event: Event2"),
+    ]
+
+    fake_cytof.close()
+
+
+def test_cytof_to_dataframe():
+    """Extract data from a CyTOF summary CSV"""
+    with open(CYTOF_PATH, "rb") as fake_cytof:
+        cytof_df = _cytof_summary_to_dataframe(fake_cytof)
+
+    expected_df = pd.DataFrame(
+        {"CTTTTPPS1.01": [1, 2], "CTTTTPPS2.01": [2, 1]}, index=["cell1", "cell2"]
+    )
+    assert expected_df.equals(cytof_df)
 
 
 def test_npx_to_dataframe():
