0f57b0d0cd09ebcf163df23d2659ef4ae24564a6
Pavel Trukhanov <pavel@ds.dfci.harvard.edu>
2020-06-30 10:58:16 -0400

    report disabled_inactive_users

--- functions/users.py
+++ functions/users.py
@@ -10,5 +10,6 @@ def disable_inactive_users(*args):
     """Disable any users who have become inactive."""
     with sqlalchemy_session() as session:
         print("Disabling inactive users...")
-        Users.disable_inactive_users(session=session)
+        disabled = Users.disable_inactive_users(session=session)
+        print("Disabled:", ", ".join([u[0] for u in disabled]) 
         print("done.")
