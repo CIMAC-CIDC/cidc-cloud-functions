36d8f1e973e8384ecd5729c3c780627093b3ea53
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-12-22 09:45:17 -0500

    Add daily function for refreshing GCS download perms (#186) (#187)
    * Add daily function for refreshing GCS download perms
    
    * Address PR comments

--- functions/users.py
+++ functions/users.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 
-from sqlalchemy.orm import Query
-from cidc_api.models import Users
+from cidc_api.models import Users, Permissions
 
 from .util import sqlalchemy_session
 
@@ -14,3 +13,26 @@ def disable_inactive_users(*args):
         for u in disabled:
             print(f"Disabled inactive: {u[0]}")
         print("done.")
+
+
+def refresh_download_permissions(*args):
+    """
+    Extend the expiry date for GCS download permissions belonging to users
+    who accessed the system in the last 2 (or so) days. If we don't do this, 
+    users whose accounts are still active might lose GCS download permission prematurely.
+    """
+    active_today = lambda q: q.filter(
+        # Provide a 3 day window to ensure we don't miss anyone
+        # if, e.g., this function fails to run on a certain day.
+        Users._accessed
+        > datetime.today() - timedelta(days=3)
+    )
+    with sqlalchemy_session() as session:
+        active_users = Users.list(
+            page_size=Users.count(session=session, filter_=active_today),
+            session=session,
+            filter_=active_today,
+        )
+        for user in active_users:
+            print(f"Refreshing IAM download permissions for {user.email}")
+            Permissions.grant_iam_permissions(user, session=session)
