50fc0d7c47e2164fa8119e3aba39ddf0dc8f9cd9
SC Van Nostrand <scvannost@gmail.com>
2022-01-20 17:01:11 -0500

    CIDC-1198 add revoke, user_email_list, docs, None handling

--- tests/functions/test_grant_permissions.py
+++ tests/functions/test_grant_permissions.py
@@ -1,5 +1,5 @@
 import functions.grant_permissions
-from functions.grant_permissions import grant_download_permissions
+from functions.grant_permissions import grant_download_permissions, permissions_worker
 from functions.settings import GOOGLE_WORKER_TOPIC
 import pytest
 from unittest.mock import MagicMock
@@ -15,19 +15,23 @@ def test_grant_download_permissions(monkeypatch):
         mock_permissions_list,
     )
 
-    users = [MagicMock(), MagicMock()]
-    user_emails = ["foo@bar.com", "user@test.com"]
-    for user, email in zip(users, user_emails):
+    user_list = [MagicMock(), MagicMock()]
+    user_email_list = ["foo@bar.com", "user@test.com"]
+    for user, email in zip(user_list, user_email_list):
         user.email = email
 
     monkeypatch.setattr(
-        functions.grant_permissions.Users, "find_by_id", lambda id, session: users.pop()
+        functions.grant_permissions.Users,
+        "find_by_id",
+        lambda id, session: user_list.pop(),
     )
 
-    mock_blob_list = MagicMock()
+    mock_blob_name_list = MagicMock()
     # need more than 100 to test chunking
-    mock_blob_list.return_value = [f"blob{n}" for n in range(100 + 50)]
-    monkeypatch.setattr(functions.grant_permissions, "get_blob_names", mock_blob_list)
+    mock_blob_name_list.return_value = [f"blob{n}" for n in range(100 + 50)]
+    monkeypatch.setattr(
+        functions.grant_permissions, "get_blob_names", mock_blob_name_list
+    )
 
     mock_encode_and_publish = MagicMock()
     monkeypatch.setattr(
@@ -47,7 +51,7 @@ def test_grant_download_permissions(monkeypatch):
 
     # incomplete/incorrect matching does nothing at all, just logging
     mock_extract_data = MagicMock()
-    mock_extract_data.return_value = str({"trial_id": "foo", "user": "baz"})
+    mock_extract_data.return_value = str({"trial_id": "foo", "user_email_list": "baz"})
     monkeypatch.setattr(
         functions.grant_permissions, "extract_pubsub_data", mock_extract_data
     )
@@ -67,7 +71,7 @@ def test_grant_download_permissions(monkeypatch):
     _, kwargs = mock_permissions_list.call_args_list[0]
     assert kwargs.get("trial_id") == "foo"
     assert kwargs.get("upload_type") == "bar"
-    mock_blob_list.assert_called_once_with(trial_id="foo", upload_type="bar")
+    mock_blob_name_list.assert_called_once_with(trial_id="foo", upload_type="bar")
 
     assert mock_encode_and_publish.call_count == 2
     call1, call2 = mock_encode_and_publish.call_args_list
@@ -75,11 +79,86 @@ def test_grant_download_permissions(monkeypatch):
 
     assert eval(call1[0][0]) == {
         "_fn": "permissions_worker",
-        "user_list": user_emails[::-1],  # pop above inverts
-        "blob_list": mock_blob_list.return_value[:100],
+        "user_email_list": user_email_list[::-1],  # pop above inverts
+        "blob_name_list": mock_blob_name_list.return_value[:100],
+        "revoke": False,
     }
     assert eval(call2[0][0]) == {
         "_fn": "permissions_worker",
-        "user_list": user_emails[::-1],  # pop above inverts
-        "blob_list": mock_blob_list.return_value[100:],
+        "user_email_list": user_email_list[::-1],  # pop above inverts
+        "blob_name_list": mock_blob_name_list.return_value[100:],
+        "revoke": False,
     }
+
+    # with revoke: True, passing revoke: True
+    # passing user_email_list doesn't get the Permissions or users
+    mock_find_user = MagicMock()
+    monkeypatch.setattr(functions.grant_permissions.Users, "find_by_id", mock_find_user)
+    mock_encode_and_publish.reset_mock()  # we're checking this
+    mock_permissions_list.reset_mock()  # shouldn't be called
+
+    mock_extract_data.return_value = str(
+        {
+            "trial_id": "foo",
+            "upload_type": "bar",
+            "user_email_list": user_email_list,
+            "revoke": True,
+        }
+    )
+    grant_download_permissions({}, None)
+
+    mock_permissions_list.assert_not_called()
+    mock_find_user.assert_not_called()
+
+    assert mock_encode_and_publish.call_count == 2
+    call1, call2 = mock_encode_and_publish.call_args_list
+    assert call1[0][1] == GOOGLE_WORKER_TOPIC and call2[0][1] == GOOGLE_WORKER_TOPIC
+
+    assert eval(call1[0][0]) == {
+        "_fn": "permissions_worker",
+        "user_email_list": user_email_list,
+        "blob_name_list": mock_blob_name_list.return_value[:100],
+        "revoke": True,
+    }
+    assert eval(call2[0][0]) == {
+        "_fn": "permissions_worker",
+        "user_email_list": user_email_list,
+        "blob_name_list": mock_blob_name_list.return_value[100:],
+        "revoke": True,
+    }
+
+
+def test_permissions_worker(monkeypatch):
+    user_email_list = ["foo@bar.com", "user@test.com"]
+    blob_name_list = [f"blob{n}" for n in range(100)]
+
+    with pytest.raises(
+        ValueError, match="user_email_list and blob_name_list must both be provided"
+    ):
+        permissions_worker()
+
+    mock_grant, mock_revoke = MagicMock(), MagicMock()
+    monkeypatch.setattr(
+        functions.grant_permissions, "grant_download_access_to_blob_names", mock_grant
+    )
+    monkeypatch.setattr(
+        functions.grant_permissions,
+        "revoke_download_access_from_blob_names",
+        mock_revoke,
+    )
+    permissions_worker(
+        user_email_list=user_email_list, blob_name_list=blob_name_list, revoke=False
+    )
+    mock_grant.assert_called_with(
+        user_email_list=user_email_list, blob_name_list=blob_name_list
+    )
+    mock_revoke.assert_not_called()
+
+    mock_grant.reset_mock()
+    permissions_worker(
+        user_email_list=user_email_list, blob_name_list=blob_name_list, revoke=True
+    )
+    mock_grant.assert_not_called()
+    mock_revoke.assert_called_with(
+        user_email_list=user_email_list, blob_name_list=blob_name_list
+    )
